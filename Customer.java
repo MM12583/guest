4b4a5e9c12e5d6a94e9f0f0cdacac67e4b9af6ef
MM12583 <lordarcher12345@gmail.com>
2021-03-31 14:57:35 +0800

    add gender and codenumber

--- Customer.java
+++ Customer.java
@@ -1,23 +1,49 @@
 package guests;
 
-import java.util.Scanner;
+import java.util.Date;
+import java.util.Scanner ;
 
 public class Customer{
 	
 	String name ;
+	String gender ;
 	String phoneNumber ;
+	String customerSerialNumber ;
 	int visitTimes ;
 	
 	public Customer(){
+		
 		name = inputName() ;
+		gender = gender() ; 
 		phoneNumber = inputPhoneNumber() ;
+		customerSerialNumber = customerSerialNumber() ;
 		visitTimes = visitTimes() ;
+			
 	}
 	
 	public String inputName(){
 		System.out.print("請輸入名稱 : ") ;
 		name = new Scanner(System.in).nextLine() ;
 		return name ;
+	}
+	
+	public String gender(){
+		boolean continueInput = true ;
+		
+		while (continueInput){	
+		System.out.print("請輸入性別 1 男性 或 2 女性 : ") ;
+		int input = new Scanner(System.in).nextInt() ;
+			if(input == 1){
+				gender = "Male" ;
+				continueInput = false ;
+			}else if (input == 2){
+				gender = "Female" ;
+				continueInput = false ;
+			}else {
+				System.out.println("請輸入 1 或 2 ") ;
+			}
+		}
+		return gender ;
 	}
 	
 	public String inputPhoneNumber(){
@@ -36,7 +62,17 @@ public class Customer{
 		return phoneNumber ;
 	}
 	
-	public int visitTimes(){
+	public String customerSerialNumber(){
+		String first = "A" ;
+		String[] codeBasic = new Date().toString().split(" ") ;
+		if (getGender() == "Female"){
+			first = "B" ;
+		}
+		customerSerialNumber = first + codeBasic[5] + codeBasic[1] + codeBasic[2] + codeBasic[3].replaceAll(":", "") ;
+		return customerSerialNumber ;
+	}
+	
+	public int visitTimes() {
 		System.out.print("請輸入來訪次數 : ") ;
 		visitTimes = new Scanner(System.in).nextInt();
 		return visitTimes ;
@@ -46,8 +82,16 @@ public class Customer{
 		return name ;
 	}
 	
+	public String getGender(){
+		return gender ;
+	}
+	
 	public String getPhoneNumber(){
 		return phoneNumber ;
+	}
+	
+	public String getcustomerSerialNumber(){
+		return customerSerialNumber ;
 	}
 	
 	public int getVisitTimes(){
--- SystemTest.java
+++ SystemTest.java
@@ -3,9 +3,16 @@ package guests;
 public class SystemTest{
 
 	public static void main(String[] args){
+		
 		Customer a = new Customer() ;
+		
 		System.out.println("消費者名稱 : " + a.getName()) ;
+		System.out.println("消費者電話 : " + a.getGender()) ;
 		System.out.println("消費者電話 : " + a.getPhoneNumber()) ;
+		System.out.println("消費者電話 : " + a.getcustomerSerialNumber()) ;
+		System.out.println("消費者電話 : " + a.getVisitTimes()) ;
+			
+		
 	}
 
 }
