29163d699451a5a325bd0a65a8504334be884719
MM12583 <lordarcher12345@gmail.com>
2021-04-11 16:27:16 +0800

    implement Serializable

--- Customer.java
+++ Customer.java
@@ -1,9 +1,10 @@
 package guests;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Scanner ;
 
-public class Customer{
+public class Customer implements Serializable{
 	
 	String name ;
 	String gender ;
